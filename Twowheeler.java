4820dac66fb9c10fd0fd3c55336d7f7993f0f58b
Vigneshmani1411 <104953836+Vigneshmani1411@users.noreply.github.com>
2022-05-12 10:19:46 +0530

    Add files via upload

---
+++ Twowheeler.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Twowheeler {
+	int noofWheels=2;
+	short noofMirrors=1;
+	long chasisNumber= 958642378456582L;
+	boolean isPunctured=false;
+	String bikeName="Yamaha";
+	double runningKms= 18897.87654;
+	
+	
+public static void main(String[] args) {
+	Twowheeler m2= new Twowheeler();
+	
+	
+	System.out.println(m2.noofWheels);
+	System.out.println(m2.noofMirrors);
+	System.out.println(m2.chasisNumber);
+	System.out.println(m2.isPunctured);
+	System.out.println(m2.bikeName);
+}
+}
